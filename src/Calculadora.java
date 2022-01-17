b15918b1ded083eb0b6002ef8bc2b1e4a753357a
Raul <urenya.jimenez@gmail.com>
2022-01-17 16:44:15 +0100

    La calculadora suma

---
+++ src/Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+
+	public static void main(String[] args) {
+
+		Calculadora calculadora = new Calculadora();
+		calculadora.suma(50.25, 20);
+
+	}
+	
+	public void suma(double x, double y) {
+		System.out.println("El resultado de la suma es " + (x + y));
+	}
+
+}
