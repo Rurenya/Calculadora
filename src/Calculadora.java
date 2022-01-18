e2762e7c3598e7664a5925cc9be773ac089117b0
Raul <urenya.jimenez@gmail.com>
2022-01-18 23:03:11 +0100

    Añadida raíz cuadrada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -15,4 +15,8 @@ public class Calculadora {
 	public void potencia(double x, double y) {
 		System.out.println("El resultado de la potencia es " + Math.pow(x, y));
 	}
+	
+	public void raiz(double n) {
+		System.out.println("La raíz cuadrada de " + n + " es: " + Math.sqrt(n));
+	}
 }
